d9282deb628751368501f765bb7ebc100e0624a3
BuildTools <mwlooker@gmail.com>
2016-02-16 15:42:08 +0100

    stuff. ugh. i'm so bad at commenting.

--- functions.js
+++ functions.js
@@ -1,27 +1,29 @@
 
 var commandList = [];
 var invalid = "Invalid command.";
+var bannedTags = ['a','img', 'object', 'iframe', 'div', 'span', 'font', 'applet', 'b', 'br', 'area', 'audio', 'aside', 'body', 'center', 'ul', 'li', 'form', 'frame', 'frameset', 'input', 'button', 'header', 'i', 'html', 'style', 'link', 'meta', 'menu', 'p', 'pre', 'ruby', 'script', 'strong', 'strike', 'textarea', 'td', 'table', 'tr', 'var'];
 
 loadFunctions();
 
 var checkCommand = function(data, socket)
 {
   if ( socket == undefined )
-   {
-     Util.debug("No commands from undf socket.");
+  {
+    Util.debug("No commands from undf socket.");
     return;
-   }
+  }
 
   var cmd;
 
   if ( data.indexOf(" ") == -1 )
-    cmd = data.trim().toLowerCase();
+    cmd = data.toString().trim();
   else
   {
-    cmd = data.substring(0,data.indexOf(" ").toLowerCase())
+    cmd = data.substring(0,data.indexOf(" "));
   }
-    data = data.substring(cmd.length).trim();
+  data = data.substring(cmd.length).trim();
 
+  cmd = cmd.toLowerCase();
   if ( commandList[cmd] )
   {
     if ( player[socket.id].level < commandList[cmd].level )
@@ -40,7 +42,7 @@ module.exports.checkCommand = checkCommand;
 var doWho = function(socket,msg) {
 
   Util.msg(socket, "<br />##3C3======================", "info");
-var count = 0;
+  var count = 0;
   for ( var x in player )
   {
     if ( player[x].state == 4)
@@ -48,7 +50,7 @@ var count = 0;
       count++;
       var str = "[%*$-3$] %*".toString();
       var arr = [ player[x].level,player[x].name ];
-       Util.msg(socket, str, "info", arr );
+      Util.msg(socket, str, "info", arr );
     }
   }
   Util.msg(socket, "<br />##3C3======================", "info");
@@ -57,14 +59,14 @@ var count = 0;
 };
 
 var doSay = function (socket, msg) {
-//  Util.info(player[socket.id].name);
-   if ( msg.trim().length == 0 )
-   {
-     Util.msg(socket,"Say what?");
-     return;
-   }
+  //  Util.info(player[socket.id].name);
+  if ( msg.trim().length == 0 )
+  {
+    Util.msg(socket,"Say what?");
+    return;
+  }
 
-   Util.msgall( "##2BC" + player[socket.id].name + " says '##7EF"+msg.toString().trim().striptag()+"##2BC'", player[socket.id].room, "chat");
+  Util.msgall( "##2BC" + player[socket.id].name + " says '##7EF"+msg.forChat()+"##2BC'", player[socket.id].room, "chat");
 };
 
 var doQuit = function (socket, msg) {
@@ -73,23 +75,23 @@ var doQuit = function (socket, msg) {
 };
 
 var doOoc = function( socket,msg) {
-     if ( msg.trim().length == 0 )
-      {
-         Util.msg(socket,"OOC what?");
-            return;
-      }
+  if ( msg.trim().length == 0 )
+  {
+    Util.msg(socket,"OOC what?");
+    return;
+  }
 
-        Util.msgall( "##19B[##BBBOOC##19B] ##EEE" + player[socket.id].name + ": "+msg.toString().trim().striptag(), null, "chat");
+  Util.msgall( "##19B[##BBBOOC##19B] ##EEE" + player[socket.id].name + ": "+msg.forChat(), null, "chat");
 };
 
 var doGossip = function( socket,msg) {
-     if ( msg.trim().length == 0 )
-      {
-         Util.msg(socket,"Gossip what?");
-            return;
-      }
+  if ( msg.trim().length == 0 )
+  {
+    Util.msg(socket,"Gossip what?");
+    return;
+  }
 
-        Util.msgall( "##90B(Gossip)##EEE " + player[socket.id].name + ": "+msg.toString().trim().striptag(), null, "chat");
+  Util.msgall( "##90B(Gossip)##EEE " + player[socket.id].name + ": "+msg.forChat(), null, "chat");
 };
 
 var doSave = function(socket) {
@@ -104,22 +106,38 @@ var doReboot = function(socket) {
     socket.emit("info", "<img src='http://kefka.redcrown.net/images/fmv/disex.png'><br />");
     socket.emit("info", "##0AFThe bright flash of the Light of Judgement covers the land!".color());
   }
-  process.exit();
+
+  setTimeout( function(){ 
+    process.exit(); }, 1500);
 
 }
 
 var doSocket = function( socket) {
   for ( var x in player )
   {
-        var str = "[%*$15$] %*$20$ (%*) - %*";
-        var arr = [ player[x].ip, player[x].name, player[x].state, player[x].id ];
-        Util.msg(socket,str.color(true),"info", arr);
+    var str = "[%*$15$] %*$20$ (%*) - %*";
+    var arr = [ player[x].ip, player[x].name, player[x].state, player[x].id ];
+    Util.msg(socket,str.color(true),"info", arr);
   }
 
 }
 
+var doPush = function( socket, msg )
+{
+  for ( var x in player )
+  {
+    if ( player[x].state == 4 )
+    {
+      player[x].sock.emit("info",msg);
+    }
+  }
+
+}
+
+
+
 function loadFunctions() {
-// Command table
+  // Command table
 
   setTimeout( function() {
 
@@ -132,13 +150,14 @@ function loadFunctions() {
 
     createCommand("reboot", doReboot,500);
     createCommand("socket", doSocket, 500);
+    createCommand("push", doPush, 500);
 
     createCommand("save", doSave);
 
     Util.info("Command list loaded");
 
   }, 1000);
-    Util.info(commandList);
+  Util.info(commandList);
 }
 
 
@@ -148,6 +167,3 @@ function createCommand(name, func, level) {
 
   commandList[name] = {name: name, funct: func, level: level};
 }
-
-
-//loadFunctions();
--- include.js
+++ include.js
@@ -6,18 +6,18 @@ GLOBAL.crypto = require('crypto-js');
 GLOBAL.async = require('async');
 var striptags = require('striptags');
 
-   GLOBAL.room = require('./room.js');
-   GLOBAL.login = require('./login.js');
-   GLOBAL.Util = require('./util.js');
-   GLOBAL.database = require('./database.js');
-   GLOBAL.io = require('./socket.js');
-   GLOBAL.functions = require('./functions.js');
-   GLOBAL.character = require('./character.js');
-   GLOBAL.config = require('./config.js');
-   GLOBAL.save = require('./save.js');
+GLOBAL.room = require('./room.js');
+GLOBAL.login = require('./login.js');
+GLOBAL.Util = require('./util.js');
+GLOBAL.database = require('./database.js');
+GLOBAL.io = require('./socket.js');
+GLOBAL.functions = require('./functions.js');
+GLOBAL.character = require('./character.js');
+GLOBAL.config = require('./config.js');
+GLOBAL.save = require('./save.js');
 
 String.prototype.cap = function() {
-   return this.charAt(0).toUpperCase() + this.toLowerCase().slice(1);
+  return this.charAt(0).toUpperCase() + this.toLowerCase().slice(1);
 }
 
 module.exports.cap = String.prototype.cap;
@@ -27,7 +27,7 @@ module.exports.cap = String.prototype.cap;
 String.prototype.color = function(pre) {
 
   var str;
-  
+
   if ( pre )
     str = "<span style='color:#fff; white-space:pre-wrap'>" + this;
   else
@@ -37,17 +37,17 @@ String.prototype.color = function(pre) {
   do {
     var pos = str.indexOf("##");
     var color = str.substr(pos+2,3);
-//    Util.debug("Pos: " + pos + " Color: " + color);
-  if ( pre )    
-    str = str.replace("##"+color, "</span><span style='white-space: pre; color:#" + color + ";'>");
-  else
-        str = str.replace("##"+color, "</span><span style='word-wrap: break-word; color:#" + color + ";'>");
+    //    Util.debug("Pos: " + pos + " Color: " + color);
+    if ( pre )    
+      str = str.replace("##"+color, "</span><span style='white-space: pre; color:#" + color + ";'>");
+    else
+      str = str.replace("##"+color, "</span><span style='word-wrap: break-word; color:#" + color + ";'>");
 
   }
   while (str.indexOf("##") != -1 );
-str = str.replace(/(?:\r\n|\r|\n|\n\r)/g, '<br \>');
+  str = str.replace(/(?:\r\n|\r|\n|\n\r)/g, '<br \>');
   return str + "</span>";
-  
+
 }
 
 module.exports.color = String.prototype.color;
@@ -57,3 +57,32 @@ String.prototype.striptag = function() {
 }
 
 module.exports.striptag = String.prototype.striptag;
+
+var emotes = [
+  [':\\\)', 'icon_smile.gif'],
+  [':\\\(', 'icon_sad.gif' ],
+  [':D', 'icon_lol.gif' ],
+  ['XD', 'icon_lol.gif' ],
+  [':heart:', 'heart.gif' ],
+  ['\\<3', 'heart.gif' ],
+  [':P', 'icon_razz.gif' ],
+  [':p', 'icon_razz.gif' ]
+
+];
+
+function applyEmotesFormat(body) {
+  for (var i = 0; i < emotes.length; i++) {
+    body = body.replace(new RegExp(emotes[i][0], 'gi'), '<img src="http:\/\/ivalicemud.com\/icon\/' + emotes[i][1] + '">');
+  }
+  return body;
+}
+
+
+String.prototype.forChat = function () {
+  var str = this.trim();
+  var str = this.replace(/\<3/g,":heart:");
+  str = str.striptag(bannedTags);
+
+  return applyEmotesFormat(str);
+}
+module.exports.forChat = String.prototype.forChat;
